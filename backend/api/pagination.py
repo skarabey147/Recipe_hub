7611100b0259d4830919eb2d8faa4b3f75dff0a2
Каракозов Егор <Skarabey147@gmail.com>
2022-04-23 16:15:16 +0300

    minor fixes

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -1,8 +1,5 @@
 from rest_framework.pagination import PageNumberPagination
 
-PAGE_SIZE = 6
-
 
 class CustomPageNumberPagination(PageNumberPagination):
-    page_size = PAGE_SIZE
     page_size_query_param = 'limit'
